7f04954d563b098a2e0f256aa5a793a78997105f
Prem-jun <prem@kku.ac.th>
2024-06-09 16:52:38 +0700

    update all

--- dlt_pipeline2_pipeline.py
+++ dlt_pipeline2_pipeline.py
@@ -1,11 +1,34 @@
 import dlt
-import pandas as pd
+# import pandas as pd
+from pyspark.sql import SparkSession
+
+# data_folder = './data/users.csv'
+# df = pd.read_csv(data_folder)
+# data  = df.to_dict(orient='records')
+ 
+# pipeline = dlt.pipeline(
+#     pipeline_name="dlt_pipeline2", destination="duckdb", dataset_name="users"
+# )
+# load_info = pipeline.run(data, table_name="users")
 
 data_folder = './data/users.csv'
-df = pd.read_csv(data_folder)
-data  = df.to_dict(orient='records')
- 
-pipeline = dlt.pipeline(
-    pipeline_name="dlt_pipeline2", destination="duckdb", dataset_name="users"
-)
-load_info = pipeline.run(data, table_name="users")
+# Initialize a SparkSession
+
+spark = SparkSession.builder \
+    .appName("ReadCSVExample")\
+    .master("local[*]") \
+    .getOrCreate()
+
+
+sparkdf = spark.read.format("csv") \
+    .option("header", "true") \
+    .load(data_folder)
+sparkdf.printSchema()
+
+# data  = df.to_dict(orient='records')
+
+# pipeline = dlt.pipeline(
+#     pipeline_name="dlt_pipeline2", destination="duckdb", dataset_name="users"
+# )
+# load_info = pipeline.run(data, table_name="users")    
+
